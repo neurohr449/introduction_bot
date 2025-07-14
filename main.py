352d239839b4fb9bada2e573463f1d85b08fff99
neurohr449 <neuroagancy69@proton.me>
2025-07-14 12:33:51 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+import asyncio
+import logging
+import sys
+import os
+import json
+from datetime import datetime, timedelta
+from aiogram import Bot, Dispatcher, html, Router, BaseMiddleware
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart, Command
+from aiogram.filters.state import StateFilter
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+
+
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+
+bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(
+    parse_mode=ParseMode.HTML))
+storage = MemoryStorage()
+router = Router()
+dp = Dispatcher(storage=storage)
+
+class UserState(StatesGroup):
+    add = State()
+    deacti = State()
+    get = State()
+
+class StateMiddleware(BaseMiddleware):
+    async def __call__(self, handler, event: Message, data: dict):
+        state = data['state']
+        current_state = await state.get_state()
+        data['current_state'] = current_state
+        return await handler(event, data)
+
+
+
+async def main() -> None:
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    dp.include_router(router)
+    dp.message.middleware(StateMiddleware())
+    bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(
+        parse_mode=ParseMode.HTML))
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
